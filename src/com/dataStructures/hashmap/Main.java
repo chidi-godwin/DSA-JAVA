879bad303cc7d23bcaf22a8a2293511b3df1e520
chidi-godwin <chidieberen1998@gmail.com>
2024-03-11 18:48:47 +0100

    solve common hashmap problems

---
+++ src/com/dataStructures/hashmap/Main.java
@@ -0,0 +1,36 @@
+package com.dataStructures.hashmap;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String sentence = "we would find the first non repeating character in this string now";
+        System.out.println(findFirstNonRepeatingCharacter(sentence));
+        System.out.println(findFirstRepeatingCharacter(sentence));
+    }
+
+    public static char findFirstNonRepeatingCharacter(String sentence) {
+        Map<Character, Integer> letterCount = new HashMap<>();
+        char[] splitLetters = sentence.toCharArray();
+
+        for (char letter: splitLetters)
+            letterCount.put(letter, letterCount.getOrDefault(letter, 0) + 1);
+
+        for (char letter: splitLetters)
+            if (letterCount.get(letter) == 1)
+                return letter;
+
+        return Character.MIN_VALUE;
+    }
+
+    public static char findFirstRepeatingCharacter(String sentence) {
+        Set<Character> charSet = new HashSet<>();
+
+        for (char letter: sentence.toCharArray())
+            if (charSet.contains(letter))
+                return letter;
+            else charSet.add(letter);
+
+        return Character.MIN_VALUE;
+    }
+}
