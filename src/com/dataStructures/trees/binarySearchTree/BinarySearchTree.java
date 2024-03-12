6c74a357a96f0313fa971da088151d41cae4d2b2
chidi-godwin <chidieberen1998@gmail.com>
2024-03-12 22:32:50 +0100

    implement BST with insert, find, and depth first traversals

---
+++ src/com/dataStructures/trees/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,121 @@
+package com.dataStructures.trees.binarySearchTree;
+
+public class BinarySearchTree {
+    private static class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node (int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(Node: %s)", value);
+        }
+    }
+
+    private Node root;
+
+
+    public void insert (int value) {
+        var node = new Node(value);
+
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        var current = root;
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+    }
+
+    public boolean find(int value) {
+        var current = root;
+
+        while (current != null) {
+            if (value < current.value)
+                current = current.leftChild;
+            else if (value > current.value)
+                current = current.rightChild;
+            else
+                return true;
+        }
+
+        return false;
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.printf("%s ", root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+
+        traverseInOrder(root.leftChild);
+        System.out.printf("%s ", root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.printf("%s ", root.value);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    public static void main(String[] args) {
+        var tree = new BinarySearchTree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+
+        tree.traversePreOrder();
+        System.out.println();
+
+        tree.traverseInOrder();
+        System.out.println();
+
+        tree.traversePostOrder();
+        System.out.println();
+    }
+}
