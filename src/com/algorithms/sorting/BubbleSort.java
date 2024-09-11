2a736c3600158a01c37501fa7aea5ea8b29ec0f3
chidi-godwin <chidieberen1998@gmail.com>
2024-09-11 05:19:12 +0100

    reset swap count on every iteration

--- src/com/algorithms/sorting/BubbleSort.java
+++ src/com/algorithms/sorting/BubbleSort.java
@@ -10,9 +10,9 @@ public class BubbleSort {
 
     public static void sort(int[] numbers) {
         int n = numbers.length;
-        int swapCount = 0;
 
         for (int i = 0; i < n - 1; i++) {
+            int swapCount = 0;
             for (int j = 1; j < n - i ; j++) {
                 if (numbers[j] < numbers[j - 1]) {
                     swap(j, j - 1, numbers);
